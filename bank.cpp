95d72b1d00d547d604c663427ebfcdda8503532a
Darkangel <wtsegars@mac.com>
2020-03-21 17:00:59 -0400

    Added bank.cpp file

---
+++ bank.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+
+struct Logger {
+    virtual ~Logger() = default;
+    virtual void log_transfer(long from, long to, double amount) = 0;
+};
+
+struct ConsoleLogger : Logger {
+    void log_transfer(long from, long to, double amount) override {
+        printf("[cons] %ld -> %ld: %f\n", from, to, amount);
+    }
+};
+
+struct FileLogger : Logger {
+    void log_transfer(long from, long to, double amount) override {
+        printf("[file] %ld,%ld,%f\n", from, to, amount);
+    }
+};
+
+struct Bank {
+    void set_logger(Logger* new_logger) {
+        logger = new_logger;
+    }
+    void make_transfer(long from, long to, double amount) {
+        if (logger) logger->log_transfer(from, to, amount);
+    }
+private:
+    Logger* logger{};
+};
